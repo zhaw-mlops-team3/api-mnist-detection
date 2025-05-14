e4f9bac96e1df75a56b91ab4c11c664459f8cf7f
noah <baumgnoa@students.zhaw.ch>
2025-05-14 09:54:34 +0200

    added detection by given artifact name

--- app/main.py
+++ app/main.py
@@ -1,15 +1,13 @@
 from fastapi import Depends, FastAPI
-import os
-
 from app.models.detect import DetectModel
 from app.services.detection import DetectionService
 
 app = FastAPI()
-WANDB_ARTIFACT_NAME = os.getenv("WANDB_ARTIFACT_NAME")
 
 
 @app.post("/detect")
 async def detect(
     request_body: DetectModel, detection_service=Depends(DetectionService)
 ):
-    return {"detection": 3}
+    detection = detection_service.detect(request_body.image)
+    return detection
--- app/services/detection.py
+++ app/services/detection.py
@@ -1,25 +1,61 @@
-from functools import lru_cache
 import wandb
 import torch
+import os
+import base64
+import torchvision.transforms as transforms
+
+from functools import lru_cache
+from io import BytesIO
+from PIL import Image
+from app.net import Net
 
 
 @lru_cache
-def get_model(artifact_name: str):
+def get_model():
     run = wandb.init()
 
-    artifact = run.use_artifact(artifact_name, type="model")
+    artifact = run.use_artifact(
+        artifact_or_name="zhaw-mlops-group3/mnist-pipeline/mnist-model:v16",
+        type="model",
+    )
     artifact_dir = artifact.download()
+    model_path = next(
+        (
+            os.path.join(artifact_dir, file)
+            for file in os.listdir(artifact_dir)
+            if file.endswith(".pth")
+        ),
+        None,
+    )
+    if model_path is None:
+        raise FileNotFoundError("No .pth file found in the artifact directory.")
 
-    model = torch.load(artifact_dir + "/model.pth")
+    model = Net()
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
 
     return model
 
 
 class DetectionService:
-    def __init__(self, artifact_name: str):
-        self._model = get_model(artifact_name)
+    def __init__(self):
+        self._model = get_model()
 
-    def detect(self, image):
-        # Perform detection using the model
-        results = self.model.detect(image)
-        return results
+    def detect(self, imageBase64):
+        image_data = base64.b64decode(imageBase64)
+        image = Image.open(BytesIO(image_data)).convert("L")  # Convert to grayscale
+
+        transform = transforms.Compose(
+            [
+                transforms.Resize((28, 28)),
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        )
+        image_tensor = transform(image).unsqueeze(0)
+
+        # Run the model
+        with torch.no_grad():
+            results = self._model(image_tensor)
+            predicted_class = torch.argmax(results, dim=1).item()
+        return predicted_class
