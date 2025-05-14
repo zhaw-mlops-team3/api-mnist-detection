c6451f391ed03ed44f62a5cf6cc55c7dc82c32ff
noah <baumgnoa@students.zhaw.ch>
2025-05-14 11:08:28 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,10 @@ from fastapi import Depends, FastAPI
 from app.models.detect import DetectModel
 from app.services.detection import DetectionService
 
+import os
+
 app = FastAPI()
+WANDB_ARTIFACT_NAME = os.getenv("WANDB_ARTIFACT_NAME")
 
 
 @app.post("/detect")
@@ -11,3 +14,8 @@ async def detect(
 ):
     detection = detection_service.detect(request_body.image)
     return detection
+
+
+@app.get("/")
+async def root():
+    return {"message": WANDB_ARTIFACT_NAME}
